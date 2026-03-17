d04aba0f68935b06716a7be89c80e88b9f024409
Anshul shrivastava <vs092241@gmail.com>
2026-03-17 04:30:24 +0000

    ✨ Add main function to calculate and display the length of a string

--- Strings/length_string.cpp
+++ Strings/length_string.cpp
@@ -1,2 +1,9 @@
 #include<iostream>
 #include<string>
+
+int main() {
+    std::string str = "Hello, World!";
+    std::cout << "Length of the string: " << str.length() << std::endl;
+    int 
+    return 0;
+}
