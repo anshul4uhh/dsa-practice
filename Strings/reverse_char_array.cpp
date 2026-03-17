002df6b2f42b174ad4ce7ee3414573f3ed2dc508
Anshul shrivastava <vs092241@gmail.com>
2026-03-17 04:38:16 +0000

    📝 Add output of the original string before processing

--- Strings/reverse_char_array.cpp
+++ Strings/reverse_char_array.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main() {
     char str[] = "Hello, World!";
     int length = sizeof(str) - 1; // Subtract 1 to exclude the null terminator
+    cout << "Original string: " << str << endl;
     cout << "Length of the character array: " << length << endl;
-    return 0;
+    // Reverse the character array
+    
 }
