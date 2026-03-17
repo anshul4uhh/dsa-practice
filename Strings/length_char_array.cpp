ec0380ac98e390c38b7c9743f06d30d4e422a840
Anshul shrivastava <vs092241@gmail.com>
2026-03-17 04:33:23 +0000

    ✨ Add calculation and output of character array length

--- Strings/length_char_array.cpp
+++ Strings/length_char_array.cpp
@@ -2,4 +2,9 @@
 #include<string>
 using namespace std;
 int main() {
+    char str[] = "Hello, World!";
+    int length = sizeof(str) - 1; // Subtract 1 to exclude the null terminator
+    cout << "Length of the character array: " << length << endl;
     
+    return 0;
+}
