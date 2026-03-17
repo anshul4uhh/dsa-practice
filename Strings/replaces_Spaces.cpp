8f64e279d29176126a2ee41fee26e9582af96eb4
Anshul shrivastava <vs092241@gmail.com>
2026-03-17 04:52:30 +0000

    🔄 Replace spaces with underscores in the string and display the modified result

--- Strings/replaces_Spaces.cpp
+++ Strings/replaces_Spaces.cpp
@@ -5,5 +5,13 @@ int main() {
     char str[] = "Hello, World!";
     int length = sizeof(str) - 1; // Subtract 1 to exclude the null terminator
     cout << "Length of the character array: " << length << endl;
+    // Replace spaces with underscores
+    for (int i = 0; i < length; i++) {
+        if (str[i] == ' ') {
+            str[i] = '_';
+        }
+    }
+    cout << "String after replacing spaces: " << str << endl;
+
     return 0;
 }
