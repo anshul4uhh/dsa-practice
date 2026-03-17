1455a224c3c204de19c08d52e838d211c3eda443
Anshul shrivastava <vs092241@gmail.com>
2026-03-17 07:11:40 +0000

    ✅ Add function to validate IPv4 addresses

--- Strings/valid_ipv4.cpp
+++ Strings/valid_ipv4.cpp
@@ -2,3 +2,20 @@
 #include<cstring>
 using namespace std;
 
+bool isValidIPv4(char str[100]) {
+    int num, dots = 0;
+    char *ptr = strtok(str, ".");
+
+    while (ptr != nullptr) {
+        if (!isdigit(*ptr)) {
+            return false; // Not a valid number
+        }
+        num = atoi(ptr);
+        if (num < 0 || num > 255) {
+            return false; // Number out of range
+        }
+        dots++;
+        ptr = strtok(nullptr, ".");
+    }
+    return dots == 4; // Valid IPv4 should have exactly 4 parts
+}
