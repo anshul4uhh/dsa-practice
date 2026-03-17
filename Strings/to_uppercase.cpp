ab3c478bfb54ba2e41b9e01a4d673a13ff55d016
Anshul shrivastava <vs092241@gmail.com>
2026-03-17 06:29:11 +0000

    🔤 Add a program to convert a string to uppercase using manual character manipulation

---
+++ Strings/to_uppercase.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+void toUpperCase(char str[100]) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - ('a' - 'A'); // Convert to uppercase
+        }
+    }
+}
+int main() {
+    char str[100];
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    toUpperCase(str);
+    cout << "String in uppercase: " << str << endl;
+
+    return 0;
+}
