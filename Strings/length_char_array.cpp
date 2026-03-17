33ac106a3717db3d1830f29ed392193c65d44bab
Anshul shrivastava <vs092241@gmail.com>
2026-03-17 04:34:32 +0000

    🔄 Refactor variable names for clarity in length calculation methods

--- Strings/length_char_array.cpp
+++ Strings/length_char_array.cpp
@@ -3,13 +3,12 @@
 using namespace std;
 int main() {
     char str[] = "Hello, World!";
-    int length = sizeof(str) - 1; // Subtract 1 to exclude the null terminator
-    cout << "Length of the character array: " << length << endl;
-    int length = 0;
-    while(str[length] != '\0') {
-        length++;
+    int length1 = sizeof(str) - 1; // Subtract 1 to exclude the null terminator
+    cout << "Length of the character array: " << length1 << endl;
+    int length2 = 0;
+    while(str[length2] != '\0') {
+        length2++;
     }
-    cout << "Length of the character array (calculated manually): " << length << endl;
-
+    cout << "Length of the character array (calculated manually): " << length2 << endl;
     return 0;
 }
