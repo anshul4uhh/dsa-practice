c0c42192e1075e635a86f8b5094348b66ada1240
Anshul shrivastava <vs092241@gmail.com>
2026-03-17 04:31:08 +0000

    🔢 Add manual string length calculation using a loop

--- Strings/length_string.cpp
+++ Strings/length_string.cpp
@@ -5,6 +5,9 @@ int main() {
     string str = "Hello, World!";
     cout << "Length of the string: " << str.length() <<endl;
     int length = 0;
-
+    for (char c : str) {
+        length++;
+    }
+    cout << "Length of the string (calculated manually): " << length << endl;
     return 0;
 }
