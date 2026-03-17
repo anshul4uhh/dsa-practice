f65f3be9b3a002679e4b9f913a26b1234a5982c1
Anshul shrivastava <vs092241@gmail.com>
2026-03-17 04:52:39 +0000

    📝 Add output of the original string before processing

--- Strings/replaces_Spaces.cpp
+++ Strings/replaces_Spaces.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     char str[] = "Hello, World!";
     int length = sizeof(str) - 1; // Subtract 1 to exclude the null terminator
+    cout << "Original string: " << str << endl;
     cout << "Length of the character array: " << length << endl;
     // Replace spaces with underscores
     for (int i = 0; i < length; i++) {
