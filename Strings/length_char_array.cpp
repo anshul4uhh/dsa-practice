aed61bf7b55a68b3bee127b38362b58388156db9
Anshul shrivastava <vs092241@gmail.com>
2026-03-17 04:34:03 +0000

    🔤 Fix incorrect length calculation by adding a loop to count characters manually

--- Strings/length_char_array.cpp
+++ Strings/length_char_array.cpp
@@ -5,6 +5,7 @@ int main() {
     char str[] = "Hello, World!";
     int length = sizeof(str) - 1; // Subtract 1 to exclude the null terminator
     cout << "Length of the character array: " << length << endl;
+    int length = 0;
     while(str[length] != '\0') {
         length++;
     }
