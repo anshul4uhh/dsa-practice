ab89435d3378a6bed56695b8dae50addca30540c
Anshul shrivastava <vs092241@gmail.com>
2026-03-17 10:36:59 +0530

    checking if a string is a palindrome or not

---
+++ Strings/palindrome_check.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+bool isPalindrome(char str[100]) {
+    int left = 0;
+    int right = strlen(str) - 1;
+
+    while (left < right) {
+        if (str[left] != str[right]) {
+            return false; // Not a palindrome
+        }
+        left++;
+        right--;
+    }
+    return true; // Is a palindrome
+}
+int main() {
+    char str[100];
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    if (isPalindrome(str)) {
+        cout << "The string is a palindrome." << endl;
+    } else {
+        cout << "The string is not a palindrome." << endl;
+    }
+
+    return 0;
+}
