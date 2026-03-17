9e9deac0db0f92a1b48636b065bd946c1f8c85fe
Anshul shrivastava <vs092241@gmail.com>
2026-03-17 04:33:51 +0000

    🔄 Fix incorrect length calculation for character array by iterating until null terminator

--- Strings/length_char_array.cpp
+++ Strings/length_char_array.cpp
@@ -5,6 +5,10 @@ int main() {
     char str[] = "Hello, World!";
     int length = sizeof(str) - 1; // Subtract 1 to exclude the null terminator
     cout << "Length of the character array: " << length << endl;
-    
+    while(str[length] != '\0') {
+        length++;
+    }
+    cout << "Length of the character array (calculated manually): " << length << endl;
+
     return 0;
 }
