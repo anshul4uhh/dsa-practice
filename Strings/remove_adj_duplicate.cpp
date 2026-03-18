39d85a01af8d3107220ccfaf3ad2291f59d7b53a
Anshul shrivastava <vs092241@gmail.com>
2026-03-18 16:49:57 +0000

    🗑️ Remove redundant loop logic in adjacent duplicate removal function

--- Strings/remove_adj_duplicate.cpp
+++ Strings/remove_adj_duplicate.cpp
@@ -9,11 +9,7 @@ string removeAdjacentDuplicates(const string& str) {
     string result;
     result += str[0]; // Add the first character to the result
 
-    for (size_t i = 1; i < str.length(); ++i) {
-        if (str[i] != str[i - 1]) {
-            result += str[i]; // Add character if it's different from the previous one
-        }
-    }
+
 
     return result;
 }
