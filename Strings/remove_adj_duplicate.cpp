13aba17ed2a886044113335d1b9021b977c67b1d
Anshul shrivastava <vs092241@gmail.com>
2026-03-18 16:48:00 +0000

    ✨ Add function to remove adjacent duplicate characters from a string

--- Strings/remove_adj_duplicate.cpp
+++ Strings/remove_adj_duplicate.cpp
@@ -1,3 +1,19 @@
 #include<iostream>
 #include<string>
 using namespace std;
+string removeAdjacentDuplicates(const string& str) {
+    if (str.empty()) {
+        return "";
+    }
+
+    string result;
+    result += str[0]; // Add the first character to the result
+
+    for (size_t i = 1; i < str.length(); ++i) {
+        if (str[i] != str[i - 1]) {
+            result += str[i]; // Add character if it's different from the previous one
+        }
+    }
+
+    return result;
+}
