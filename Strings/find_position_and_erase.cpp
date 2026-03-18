245fc7c937188031de81b4d3e5f6cbbcca0fe29d
Anshul shrivastava <vs092241@gmail.com>
2026-03-18 17:29:22 +0000

    ✨ Add variable to store substring length for improved readability

--- Strings/find_position_and_erase.cpp
+++ Strings/find_position_and_erase.cpp
@@ -7,6 +7,7 @@ string erase_substring(string& str,string& part){
     if(str.empty()){
         return " ";
     }
+    int length = part.length();
     while(str.find(part) != string::npos){
         int pos = str.find(part);
     }
