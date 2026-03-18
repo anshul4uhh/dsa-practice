3534a04efab3d007b2eba8b2d57335af83c78abb
Anshul shrivastava <vs092241@gmail.com>
2026-03-18 16:48:19 +0000

    🖋️ Add output of the original string before processing

--- Strings/remove_adj_duplicate.cpp
+++ Strings/remove_adj_duplicate.cpp
@@ -21,7 +21,7 @@ int main() {
     string input;
     cout << "Enter a string: ";
     getline(cin, input);
-
+    cout << "Original string: " << input << endl;
     string output = removeAdjacentDuplicates(input);
     cout << "String after removing adjacent duplicates: " << output << endl;
 
