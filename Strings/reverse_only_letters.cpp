362d4691096f20c719fc59e6e4f90b7c84bf42a7
Anshul shrivastava <vs092241@gmail.com>
2026-03-19 07:17:15 +0000

    🔄 Reverse only letters in the string logic

---
+++ Strings/reverse_only_letters.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string reverseOnlyLetters(string s) {
+        int n = s.length();
+        int l=0,r=n-1;
+        while(l<r){
+            if(isalpha(s[l]) && isalpha(s[r])){
+                char temp = s[l];
+                s[l]=s[r];
+                s[r]=temp;
+                l++,r--;
+            }
+            else{
+                if(!isalpha(s[l])) l++;
+                else r--;
+            }
+        }
+        return s;
+    }
+    
