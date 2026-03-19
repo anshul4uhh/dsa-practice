fa0fac7a455c556737f12e7c7f81372c2415fc29
Anshul shrivastava <vs092241@gmail.com>
2026-03-19 06:32:21 +0000

    🔤 Fix character indexing and loop range to handle full ASCII set in anagram validation

--- Strings/valid_anagram.cpp
+++ Strings/valid_anagram.cpp
@@ -7,10 +7,10 @@ bool isAnagram(string s, string t) {
     }
     int count[256] = {0};
     for(int i=0;i<s.length();i++){
-        count[s[i]-'a']++;
-        count[t[i]-'a']--;
+        count[s[i]]++;
+        count[t[i]]--;
     }
-    for(int i=0;i<26;i++){
+    for(int i=0;i<256;i++){
         if(count[i] != 0){
             return false;
         }
