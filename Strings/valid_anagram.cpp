2fdda0559d59f1d278b12e9d93bfcb6bae0436b2
Anshul shrivastava <vs092241@gmail.com>
2026-03-19 06:32:31 +0000

    📝 Add user input handling and main function to check if two strings are anagrams

--- Strings/valid_anagram.cpp
+++ Strings/valid_anagram.cpp
@@ -17,3 +17,16 @@ bool isAnagram(string s, string t) {
     }
     return true;
 }
+int main() {
+    string s, t;
+    cout << "Enter the first string: ";
+    getline(cin, s);
+    cout << "Enter the second string: ";
+    getline(cin, t);
+    if (isAnagram(s, t)) {
+        cout << "The strings are anagrams." << endl;
+    } else {
+        cout << "The strings are not anagrams." << endl;
+    }
+    return 0;
+}
