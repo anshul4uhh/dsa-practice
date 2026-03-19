5c9406eb3267c22709280dbe5988658cfdb65578
Anshul shrivastava <vs092241@gmail.com>
2026-03-19 06:01:18 +0000

    ✨ Add function to count palindromic substrings

--- Strings/palindromic_substring.cpp
+++ Strings/palindromic_substring.cpp
@@ -10,3 +10,13 @@ int expandAroundCenter(const string& s, int left, int right) {
     return right - left - 1; // Length of the palindrome
 }
 
+int countPalindromicSubstrings(const string& s) {
+    int count = 0;
+    for (int i = 0; i < s.length(); i++) {
+        // Odd length palindromes
+        count += expandAroundCenter(s, i, i);
+        // Even length palindromes
+        count += expandAroundCenter(s, i, i + 1);
+    }
+    return count;
+}
