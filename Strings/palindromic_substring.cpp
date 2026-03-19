b0e4ff83f1f77afa9452b0fdb2ff31ea594ff8dd
Anshul shrivastava <vs092241@gmail.com>
2026-03-19 06:01:06 +0000

    ✨ Add function to calculate palindrome length by expanding around center

--- Strings/palindromic_substring.cpp
+++ Strings/palindromic_substring.cpp
@@ -2,4 +2,11 @@
 #include<string>    
 using namespace std;
 
-int exp
+int expandAroundCenter(const string& s, int left, int right) {
+    while (left >= 0 && right < s.length() && s[left] == s[right]) {
+        left--;
+        right++;
+    }
+    return right - left - 1; // Length of the palindrome
+}
+
