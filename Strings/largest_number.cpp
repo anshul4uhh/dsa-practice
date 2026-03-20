16991f99bb1288ab5b62adb1a16ea10c585091bd
Anshul shrivastava <vs092241@gmail.com>
2026-03-20 07:52:16 +0000

    ✨ Add custom string comparison function for arranging numbers

--- Strings/largest_number.cpp
+++ Strings/largest_number.cpp
@@ -2,3 +2,22 @@
 #include<string>
 using namespace std;
 
+bool compare(string a, string b){
+    string ab = a+b;
+    string ba = b+a;
+    return ab>ba;
+}
+
+string largestNumber(vector<int>& nums) {
+    vector<string> strNums;
+    for(int num : nums){
+        strNums.push_back(to_string(num));
+    }
+    sort(strNums.begin(),strNums.end(),compare);
+    string result;
+    for(string str : strNums){
+        result += str;
+    }
+    if(result[0] == '0') return "0";
+    return result;
+}
