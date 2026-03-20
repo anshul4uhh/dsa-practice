71ab02b0c0d6a1f8aaba078fc3a2b36ceae8b822
Anshul shrivastava <vs092241@gmail.com>
2026-03-20 15:50:21 +0000

    ✨ Add in-place string compression function

--- Strings/string_compression.cpp
+++ Strings/string_compression.cpp
@@ -1,10 +1,28 @@
 #include<iostream>
 #include<string>
 #include<algorithm>
-#include
+#include<vector>
 using namespace std;
 
-
+string compressString_using_inplace(string s) {
+    int n = s.length();
+    int writeIndex = 0;
+    for (int i = 0; i < n; i++) {
+        int count = 1;
+        while (i + 1 < n && s[i] == s[i + 1]) {
+            count++;
+            i++;
+        }
+        s[writeIndex++] = s[i];
+        if (count > 1) {
+            string countStr = to_string(count);
+            for (char c : countStr) {
+                s[writeIndex++] = c;
+            }
+        }
+    }
+    return s.substr(0, writeIndex);
+}
 
 string compressString(string s) {
     string ans="";
