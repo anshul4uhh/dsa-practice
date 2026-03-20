626be892d30e5757ccd50f96e5a42dc75074ff78
Anshul shrivastava <vs092241@gmail.com>
2026-03-20 15:42:40 +0000

    ✨ Add string compression function implementation

---
+++ Strings/string_compression.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+string compressString(string s) {
+    string ans="";
+    int n = s.length();
+    for(int i=0;i<n;i++){
+        int count=1;
+        while(i+1<n && s[i]==s[i+1]){
+            count++;
+            i++;
+        }
+        ans+=s[i];
+        if(count>1){
+            ans+=to_string(count);
+        }
+    }
+    return ans;
+}
