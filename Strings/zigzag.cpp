3ef813b34d627ef97f976593208cd71201bc9400
Anshul shrivastava <vs092241@gmail.com>
2026-03-20 08:30:58 +0000

    ✨ Add zigzag string conversion function

--- Strings/zigzag.cpp
+++ Strings/zigzag.cpp
@@ -2,3 +2,17 @@
 #include<string>
 using namespace std;
 
+string convert(string s, int numRows) {
+        if(numRows==1) return s;
+        string ans="";
+        int n = s.length();
+        for(int i=0;i<numRows;i++){
+            for(int j=i;j<n;j+=2*numRows-2){
+                ans+=s[j];
+                if(i!=0 && i!=numRows-1 && j+2*numRows-2-2*i<n){
+                    ans+=s[j+2*numRows-2-2*i];
+                }
+            }
+        }
+        return ans;
+    }
