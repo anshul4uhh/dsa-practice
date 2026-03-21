3bcc1a53a31a5aed0a6907017d5fce12217692d0
Anshul shrivastava <vs092241@gmail.com>
2026-03-21 09:38:20 +0000

    🧹 Add missing newline for code readability

--- Pointer/basics_of_pointer.cpp
+++ Pointer/basics_of_pointer.cpp
@@ -8,6 +8,7 @@ int main() {
     cout << "Address of a: " << &a << endl; // Output: address of a
     cout << "Value stored in ptr (address of a): " << ptr << endl; // Output: address of a
     cout << "Value pointed to by ptr (value of a): " << *ptr << endl; // Output: 10
+    
 
     return 0;
 }
