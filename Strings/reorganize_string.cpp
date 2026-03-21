291a0463ad1af74326a5c4d8277e521f088b1c21
Anshul shrivastava <vs092241@gmail.com>
2026-03-21 04:25:42 +0000

    🔄 Simplify character frequency calculation and reorganize logic for string rearrangement

--- Strings/reorganize_string.cpp
+++ Strings/reorganize_string.cpp
@@ -4,35 +4,23 @@
 #include<algorithm>
 using namespace std;
 string reorganizeString(string s) {
-    unordered_map<char, int> charCount;
+    int charCount[26] = {0};
     for (char c : s) {
-        charCount[c]++;
+        charCount[c - 'a']++;
     }
-    
-    vector<pair<char, int>> charFreq(charCount.begin(), charCount.end());
-    sort(charFreq.begin(), charFreq.end(), [](const pair<char, int>& a, const pair<char, int>& b) {
-        return a.second > b.second;
-    });
-    
-    if (charFreq[0].second > (s.length() + 1) / 2) {
-        return "";
-    }
-    
-    string result(s.length(), ' ');
-    int index = 0;
-    
-    for (const auto& pair : charFreq) {
-        char c = pair.first;
-        int count = pair.second;
-        
-        for (int i = 0; i < count; i++) {
-            result[index] = c;
-            index += 2;
-            if (index >= s.length()) {
-                index = 1;
-            }
+    int maxCount = 0;
+    char maxChar;
+    for (int i = 0; i < 26; i++) {
+        if (charCount[i] > maxCount) {
+            maxCount = charCount[i];
+            maxChar = 'a' + i;
         }
     }
+    if (maxCount > (s.length() + 1) / 2) {
+        return "";
+    }
+  
+
     
-    return result;
+    return s;
 }
