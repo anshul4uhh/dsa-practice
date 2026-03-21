139dce9e8b26833ef85a38808b553ba713c8439c
Anshul shrivastava <vs092241@gmail.com>
2026-03-21 09:38:10 +0000

    🖇️ Add basic pointer demonstration with value and address outputs

--- Pointer/basics_of_pointer.cpp
+++ Pointer/basics_of_pointer.cpp
@@ -1,2 +1,13 @@
 #include<iostream>
+using namespace std;
+int main() {
+    int a = 10;
+    int* ptr = &a; // ptr holds the address of a
 
+    cout << "Value of a: " << a << endl; // Output: 10
+    cout << "Address of a: " << &a << endl; // Output: address of a
+    cout << "Value stored in ptr (address of a): " << ptr << endl; // Output: address of a
+    cout << "Value pointed to by ptr (value of a): " << *ptr << endl; // Output: 10
+
+    return 0;
+}
