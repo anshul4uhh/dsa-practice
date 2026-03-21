022d4a256d51e5707227f8d4d8cbb197cf18a9e5
Anshul shrivastava <vs092241@gmail.com>
2026-03-21 04:00:27 +0000

    🔤 Add function to group anagrams from a list of strings

--- Strings/group_anagram.cpp
+++ Strings/group_anagram.cpp
@@ -4,3 +4,19 @@
 #include<algorithm>
 
 using namespace std;
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    unordered_map<string, vector<string>> anagramMap;
+    
+    for (const string& str : strs) {
+        string sortedStr = str;
+        sort(sortedStr.begin(), sortedStr.end());
+        anagramMap[sortedStr].push_back(str);
+    }
+    
+    vector<vector<string>> result;
+    for (const auto& pair : anagramMap) {
+        result.push_back(pair.second);
+    }
+    
+    return result;
+}
