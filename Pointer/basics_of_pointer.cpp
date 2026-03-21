2b8529ce3304d42028296f60637f8f515e0d574b
Anshul shrivastava <vs092241@gmail.com>
2026-03-21 09:41:59 +0000

    🖋️ Add output for pointer size in basics_of_pointer.cpp

--- Pointer/basics_of_pointer.cpp
+++ Pointer/basics_of_pointer.cpp
@@ -10,7 +10,7 @@ int main() {
     cout << "Value pointed to by ptr (value of a): " << *ptr << endl; // Output: 10
     cout << "Address of ptr: " << &ptr << endl; // Output: address of ptr
     cout<< "Size of int: " << sizeof(int) << " bytes" << endl; // Output: size of int
-    c
+    cout << "Size of pointer: " << sizeof(ptr) << " bytes" << endl; // Output: size of pointer (typically 4 or 8 bytes depending on the system)
 
     return 0;
 }
