d24eea7ab2fe6bf5a932991c273aa0bc7289cf1c
Anshul shrivastava <vs092241@gmail.com>
2026-03-21 04:00:37 +0000

    🖥️ Add main function to demonstrate grouping anagrams functionality

--- Strings/group_anagram.cpp
+++ Strings/group_anagram.cpp
@@ -20,3 +20,18 @@ vector<vector<string>> groupAnagrams(vector<string>& strs) {
     
     return result;
 }
+
+int main() {
+    vector<string> input = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    vector<vector<string>> groupedAnagrams = groupAnagrams(input);
+    
+    cout << "Grouped Anagrams:" << endl;
+    for (const auto& group : groupedAnagrams) {
+        for (const string& str : group) {
+            cout << str << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
