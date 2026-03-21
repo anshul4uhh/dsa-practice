eec5e2e08cfb16c4f6167167ed08fa26d2216dcd
Anshul shrivastava <vs092241@gmail.com>
2026-03-21 04:52:26 +0000

    ✨ Simplify and optimize logic for finding the longest palindromic substring

--- Strings/longest_palindromic_substring.cpp
+++ Strings/longest_palindromic_substring.cpp
@@ -1,51 +1,46 @@
 #include<iostream>
-#include<string>    
+#include<string>
 using namespace std;
 
 string expandAroundCenter(const string& s, int left, int right) {
-    int count = 0;
-    string longestPalindrome;
-    int maxLength = 0;
-    while (left >= 0 && right < s.length() && s[left] == s[right]) {
-        count++;
-        int currentLength = right - left + 1;
-        if (currentLength > maxLength) {
-            maxLength = currentLength;
-            longestPalindrome = s.substr(left, currentLength);
-        }
+    while(left >= 0 && right < s.length() && s[left] == s[right]) {
         left--;
         right++;
     }
-    cout << "Longest palindromic substring: " << longestPalindrome << endl;
-    return longestPalindrome;
+    
+    return s.substr(left + 1, right - left - 1);
 }
 
-int count_length_PalindromicSubstrings(const string& s) {
-    string longestPalindromeOverall;
-    string longestPalindrome_odd;
-    string longestPalindrome_even;
-    for (int i = 0; i < s.length(); i++) {
-        // Odd length palindromes
-        longestPalindrome_odd = expandAroundCenter(s, i, i);
-        // Even length palindromes
-        longestPalindrome_even = expandAroundCenter(s, i, i + 1);
+string longestPalindrome(string s) {
+    string longest = "";
+
+    for(int i = 0; i < s.length(); i++) {
+        
+        // odd length palindrome
+        string odd = expandAroundCenter(s, i, i);
+
+        // even length palindrome
+        string even = expandAroundCenter(s, i, i + 1);
+
+        if(odd.length() > longest.length())
+            longest = odd;
+
+        if(even.length() > longest.length())
+            longest = even;
     }
-    if(longestPalindrome_odd.length() > longestPalindrome_even.length()) {
-        longestPalindromeOverall = longestPalindrome_odd;
-    } else {
-        longestPalindromeOverall = longestPalindrome_even;
-    }
-    cout << "Longest palindromic substring overall: " << longestPalindromeOverall << endl;
-    return longestPalindromeOverall.length();
+
+    return longest;
 }
 
 int main() {
     string input;
     cout << "Enter a string: ";
     getline(cin, input);
-    cout << "Original string: " << input << endl;
-    int output = count_length_PalindromicSubstrings(input);
-    cout << "Length of longest palindromic substring: " << output << endl;
+
+    string result = longestPalindrome(input);
+
+    cout << "Longest Palindromic Substring: " << result << endl;
+    cout << "Length: " << result.length() << endl;
 
     return 0;
 }
