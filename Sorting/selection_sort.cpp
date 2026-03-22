92d7cc2f8165f90fd22952b7f406cf5d75ff49b8
Anshul shrivastava <vs092241@gmail.com>
2026-03-22 13:52:04 +0000

    ✨ Implement selection sort algorithm

--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -1,3 +1,17 @@
 #include<iostream>
 using namespace std;
-void 
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i; // Assume the minimum is the first element
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j; // Update minIndex if a smaller element is found
+            }
+        }
+        // Swap the found minimum element with the first element
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+}
+
