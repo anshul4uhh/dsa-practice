a26335be05131b69156a1357ae3525d0df0d4364
Anshul shrivastava <vs092241@gmail.com>
2026-03-25 19:22:57 +0000

    🧩 Add palindrome checking function to support string validation

--- Recursion/all_subsequence.cpp
+++ Recursion/all_subsequence.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
 
+bool isPalindrome(char str[100]) {
+    int left = 0;
+    int right = strlen(str) - 1;
+
+    while (left < right) {
+        if (str[left] != str[right]) {
+            return false; // Not a palindrome
+        }
+        left++;
+        right--;
+    }
+    return true; // Is a palindrome
+}
+
 void generate_subsequences(string str, string output, int index) {
     if(index >= str.length()) {
         cout << output << endl; // Base case: print the current subsequence
