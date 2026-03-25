5bea4a4c7cf111417a65b61badc13d2f1b57858b
Anshul shrivastava <vs092241@gmail.com>
2026-03-25 12:12:16 +0000

    ✨ Add main function to calculate and display the number of ways to climb stairs

--- Recursion/steps_stairs.cpp
+++ Recursion/steps_stairs.cpp
@@ -11,3 +11,10 @@ int count_ways(int n) {
     }
     return count_ways(n - 1) + count_ways(n - 2); // Recursive case: sum of ways to climb n-1 and n-2 steps
 }
+int main() {
+    int steps;
+    cout << "Enter the number of steps: ";
+    cin >> steps;
+    cout << "Number of ways to climb " << steps << " steps: " << count_ways(steps) << endl;
+    return 0;
+}
