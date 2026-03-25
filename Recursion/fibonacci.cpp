383bd98b037d5b900980094bfb868398e75b0cff
Anshul shrivastava <vs092241@gmail.com>
2026-03-25 12:06:35 +0000

    🐛 Fix missing return value and implement recursive logic for Fibonacci calculation

--- Recursion/fibonacci.cpp
+++ Recursion/fibonacci.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int fibonacci(int n){
     if(n==0||n==1){
-        return;
+        return n; // Base case: fibonacci of 0 is 0 and fibonacci of 1 is 1
     }
+    return fibonacci(n-1) + fibonacci(n-2); // Recursive case
      
 }
