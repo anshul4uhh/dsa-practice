a5e4b7fd48bb7c011811720c0e0f445a6bce436d
Anshul shrivastava <vs092241@gmail.com>
2026-03-25 19:14:21 +0000

    ✨ Add recursive function to generate all subsequences of a string

--- Recursion/all_subsequence.cpp
+++ Recursion/all_subsequence.cpp
@@ -1,4 +1,13 @@
 #include<iostream>
 using namespace std;
 
-void 
+void generate_subsequences(string str, string current, int index) {
+    if(index == str.length()) {
+        cout << current << endl; // Base case: print the current subsequence
+        return;
+    }
+    // Include the current character and move to the next index
+    generate_subsequences(str, current + str[index], index + 1);
+    // Exclude the current character and move to the next index
+    generate_subsequences(str, current, index + 1);
+}
