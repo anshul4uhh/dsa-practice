977e043a322575128042b196b5cd6c9cde161ed3
Anshul shrivastava <vs092241@gmail.com>
2026-03-25 19:21:57 +0000

    ✨ Add function to generate and print all subsequences of a string

--- Recursion/subsequence_palindrome_check.cpp
+++ Recursion/subsequence_palindrome_check.cpp
@@ -1,3 +1,20 @@
 #include<iostream>
 using namespace std;
 
+void generate_subsequences(string str, string output, int index) {
+    if(index >= str.length()) {
+        cout << output << endl; // Base case: print the current subsequence
+        return;
+    }
+    // Include the current character and move to the next index
+    generate_subsequences(str, output, index + 1);
+    output.push_back(str[index]); // Add the current character to the output
+    // Exclude the current character and move to the next index
+    generate_subsequences(str, output, index + 1);
+}
+
+int main() {
+    string str = "abc";
+    generate_subsequences(str, "", 0);
+    return 0;
+}
