599af02ca5c47eb3959dfaba017b9d1c7dc38cfc
Anshul shrivastava <vs092241@gmail.com>
2026-03-25 12:14:07 +0000

    🧹 Fix minor formatting inconsistency in base case condition

--- Recursion/steps_stairs.cpp
+++ Recursion/steps_stairs.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 int count_ways(int n) {
-    if(n == 0) {
+    if(n == 0 ||) {
         return 1; // Base case: there's one way to climb zero steps (do nothing)
     }
     if(n < 0) {
