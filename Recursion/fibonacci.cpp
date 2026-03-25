1349a3d713d1f3b3d0a797e881d2902a0388b94d
Anshul shrivastava <vs092241@gmail.com>
2026-03-25 12:06:46 +0000

    🌀 Add main function to calculate Fibonacci

--- Recursion/fibonacci.cpp
+++ Recursion/fibonacci.cpp
@@ -6,5 +6,15 @@ int fibonacci(int n){
         return n; // Base case: fibonacci of 0 is 0 and fibonacci of 1 is 1
     }
     return fibonacci(n-1) + fibonacci(n-2); // Recursive case
-     
+}
+int main(){
+    int number;
+    cout << "Enter a number to find its Fibonacci: ";
+    cin >> number;
+    if(number < 0) {
+        cout << "Fibonacci is not defined for negative numbers." << endl;
+    } else {
+        cout << "Fibonacci of " << number << " is " << fibonacci(number) << endl;
+    }
+    return 0;
 }
