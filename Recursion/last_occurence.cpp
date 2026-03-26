ad372f07d03758b29e9077eb264f8cc28f9cf3cc
Anshul shrivastava <vs092241@gmail.com>
2026-03-26 05:04:36 +0000

    🔄 Add recursive function to find the last occurrence of a character in a string and implement main function for testing

--- Recursion/last_occurence.cpp
+++ Recursion/last_occurence.cpp
@@ -1,3 +1,21 @@
 #include<iostream>
 using namespace std;
 
+int last_occurrence(string str, char target, int index) {
+    if(index < 0) {
+        return -1; // Base case: target not found
+    }
+    if(str[index] == target) {
+        return index; // Target found at the current index
+    }
+    // Recursive call to check the previous index
+    return last_occurrence(str, target, index - 1);
+}
+
+int main() {
+    string str = "hello";
+    char target = 'l';
+    int index = str.length() - 1;
+    cout << last_occurrence(str, target, index) << endl;
+    return 0;
+}
