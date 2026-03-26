ce0f626c096d0132758a2d997b2bd8e93ac9288d
Anshul shrivastava <vs092241@gmail.com>
2026-03-26 05:48:13 +0000

    ➕ Refactor recursive string addition logic to include carry handling

--- Recursion/add_strings.cpp
+++ Recursion/add_strings.cpp
@@ -8,6 +8,7 @@ void add_strings(string str1, string str2, string result, int index1, int index2
     }
     int digit1 = (index1 < str1.length()) ? str1[str1.length() - 1 - index1] - '0' : 0;
     int digit2 = (index2 < str2.length()) ? str2[str2.length() - 1 - index2] - '0' : 0;
+    
     int sum = digit1 + digit2 + (result.empty() ? 0 : result[0] - '0');
     result.insert(result.begin(), (sum % 10) + '0'); // Add the last digit of the sum to the result
     add_strings(str1, str2, result, index1 + 1, index2 + 1); // Recursive call for the next digits
