30a593aea8406a14f2e50808c423c57df7bfd8d0
Anshul shrivastava <vs092241@gmail.com>
2026-03-26 05:12:36 +0000

    ✨ Add main function to demonstrate string reversal using recursion

--- Recursion/reverse_str.cpp
+++ Recursion/reverse_str.cpp
@@ -11,3 +11,10 @@ void reverse_string(string str, string output, int index) {
     reverse_string(str, output, index + 1);
     output.push_back(str[index]); // Add the current character to the output after the recursive call
 }
+
+int main() {
+    string str = "hello";
+    string output = "";
+    reverse_string(str, output, 0);
+    return 0;
+}
