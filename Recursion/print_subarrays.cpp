e1c7e72c6d1f3e92b31e23c5931a5683196cc72c
Anshul shrivastava <vs092241@gmail.com>
2026-03-26 07:35:29 +0000

    ✨ Add main function to demonstrate printing all subarrays of an array

--- Recursion/print_subarrays.cpp
+++ Recursion/print_subarrays.cpp
@@ -17,3 +17,10 @@ void print_subarrays(int arr[], int n, int start, int end) {
     print_subarrays(arr, n, start + 1, end); // Move to the next starting index
 }
 
+int main() {
+    int arr[] = {1, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "All subarrays of the given array are:" << endl;
+    print_subarrays(arr, n, 0, 0);
+    return 0;
+}
