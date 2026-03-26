38707edbc7472fb9ed0b4e8463691ce998237d81
Anshul shrivastava <vs092241@gmail.com>
2026-03-26 07:18:41 +0000

    🧹 Add base case and output for removing occurrences in recursion

--- Recursion/remove_subarrays.cpp
+++ Recursion/remove_subarrays.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 void remove_occurences(string& s, string& part){
+    int index = s.find(part);
+    if(index == string::npos) {
+        cout << "Final string after removing all occurrences: " << s << endl;
+        return; // Base case: no more occurrences found
+    }
     
 }
