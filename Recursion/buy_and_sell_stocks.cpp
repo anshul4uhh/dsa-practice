660e3dfcf39bb3810d4f24979304899383f73489
Anshul shrivastava <vs092241@gmail.com>
2026-04-03 01:50:31 +0000

    🛠️ Improve stock trading logic by updating buy price and maximizing profit calculation

--- Recursion/buy_and_sell_stocks.cpp
+++ Recursion/buy_and_sell_stocks.cpp
@@ -7,5 +7,7 @@ void buy_and_sell_stocks_helper(vector<int>& prices, int index, int buy_price, i
         cout << "Profit: " << profit << endl; // Base case: end of price list
         return;
     }
+    if(prices[index]<buy_price) buy_price = prices[index]; // Update buy price if current price is lower
+    else profit = max(profit, prices[index] - buy_price); // Update profit if selling at current price is better
     
 }
