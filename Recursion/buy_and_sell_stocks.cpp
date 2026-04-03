2442dce7f73716006c82514335bce1bbab8b01fc
Anshul shrivastava <vs092241@gmail.com>
2026-04-03 01:50:55 +0000

    🛠️ Fix incorrect parameter type in recursive helper function

--- Recursion/buy_and_sell_stocks.cpp
+++ Recursion/buy_and_sell_stocks.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-void buy_and_sell_stocks_helper(vector<int>& prices, int index, intbuy_price, int&profit) {
+void buy_and_sell_stocks_helper(vector<int>& prices, int index, int&buy_price, int&profit) {
     if(index >= prices.size()) {
         cout << "Profit: " << profit << endl; // Base case: end of price list
         return;
@@ -11,3 +11,4 @@ void buy_and_sell_stocks_helper(vector<int>& prices, int index, intbuy_price, in
     else profit = max(profit, prices[index] - buy_price); // Update profit if selling at current price is better
     buy_and_sell_stocks_helper(prices, index + 1, buy_price, profit); // Recursive call to check next price
 }
+int ma
