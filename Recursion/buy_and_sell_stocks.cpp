331b0cbe5ff7aa44df485d1295d1253b83e252f0
Anshul shrivastava <vs092241@gmail.com>
2026-04-03 01:50:45 +0000

    🔄 Fix profit parameter to be passed by reference in recursive stock trading helper function

--- Recursion/buy_and_sell_stocks.cpp
+++ Recursion/buy_and_sell_stocks.cpp
@@ -2,12 +2,12 @@
 #include<vector>
 using namespace std;
 
-void buy_and_sell_stocks_helper(vector<int>& prices, int index, int buy_price, int profit) {
+void buy_and_sell_stocks_helper(vector<int>& prices, int index, intbuy_price, int&profit) {
     if(index >= prices.size()) {
         cout << "Profit: " << profit << endl; // Base case: end of price list
         return;
     }
     if(prices[index]<buy_price) buy_price = prices[index]; // Update buy price if current price is lower
     else profit = max(profit, prices[index] - buy_price); // Update profit if selling at current price is better
-    
+    buy_and_sell_stocks_helper(prices, index + 1, buy_price, profit); // Recursive call to check next price
 }
