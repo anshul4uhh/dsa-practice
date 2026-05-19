447bf096a77b7e44a535c3ff97fa92820f5fea22
Anshul shrivastava <vs092241@gmail.com>
2026-05-19 09:34:55 +0000

    May 19, 2026, 3:04 PM

--- unthinkable_practice/prime_sum.cpp
+++ unthinkable_practice/prime_sum.cpp
@@ -6,7 +6,24 @@ using namespace std;
 void mark_prime(int n,vector<int>&prime){
     prime[0]=prime[1]=0;
     for(int i=2;i*i<=n;i++){
-        if(prime[])
+        if(prime[i]){
+            int j=i*2;
+            while(j<=n){
+                prime[j]=0;
+                j+=i;
+            }
+        }
     }
+}
 
+int main(){
+    int n;
+    cout<<"emter value of n: ";
+    cin>>n;
+    vector<int>prime(n+1,1);
+    mark_prime(n,prime);
+    for(int i=0;i<=n;i++){
+        if(prime[i]) cout<<i<<" ";
+    }
+    return 0;
 }
