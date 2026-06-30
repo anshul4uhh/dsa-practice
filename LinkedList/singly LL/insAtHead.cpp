d1eaa60187c5fc77131b42e598e24e68a6fce606
Anshul shrivastava <vs092241@gmail.com>
2026-06-30 03:59:58 +0000

    Jun 30, 2026, 9:29 AM

--- LinkedList/singly LL/insAtHead.cpp
+++ LinkedList/singly LL/insAtHead.cpp
@@ -1,1 +1,14 @@
 #include<iostream>
+#include <algorithm>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
