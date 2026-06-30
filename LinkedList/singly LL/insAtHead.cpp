56e70100de83cb5d7552f6990309a29485687ed3
Anshul shrivastava <vs092241@gmail.com>
2026-06-30 04:15:40 +0000

    Jun 30, 2026, 9:45 AM

--- LinkedList/singly LL/insAtHead.cpp
+++ LinkedList/singly LL/insAtHead.cpp
@@ -27,6 +27,19 @@ void insertAtHead(Node*& head, int value) {
     head = newNode;
 }
 
+void insertAtTail(Node*& head, int value) {
+    Node* newNode = new Node(value);
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
 int main() {
     int n;
     cout << "Enter number of nodes: ";
