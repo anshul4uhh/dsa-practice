3ae98dc1206515e96a3e686d5dd49baa1ea18b15
Anshul shrivastava <vs092241@gmail.com>
2026-06-30 04:14:52 +0000

    Jun 30, 2026, 9:44 AM

--- LinkedList/singly LL/insAtHead.cpp
+++ LinkedList/singly LL/insAtHead.cpp
@@ -12,6 +12,15 @@ public:
     }
 };
 
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
 int main() {
     int n;
     cout << "Enter number of nodes: ";
